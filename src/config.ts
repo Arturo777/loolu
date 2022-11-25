8ee5271e812583a077a3c47d056857dee1470a9f
jsorianoalvarez <jorge.soriano@vinneren.com.mx>
2022-11-24 18:03:06 -0600

    s

--- src/config.ts
+++ src/config.ts
@@ -28,7 +28,7 @@ export const AWS_API = {
 
 export const STYRK_API = 'http://styrk-vinneren.us-east-1.elasticbeanstalk.com:8093';
 export const STYRK_TOKEN =
-    'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzdHlya0pXVCIsInN1YiI6Im9odWl0cm9uIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY2OTI2MDQ3NiwiZXhwIjoxNjY5MjY2NDc2fQ.NvZ4VP5N-9Med7we-Ux26_q2qu1AVSEoDePh7P4jruA1kBAxey-AxXFWvbR2dRS_dHJXf_rucQaD1dOUuFfN8w';
+    'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJzdHlya0pXVCIsInN1YiI6Im9odWl0cm9uIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY2OTMzMTc5NywiZXhwIjoxNjY5MzM3Nzk3fQ.2z_Oj2xsXhL0jlyMtcVF0QtZ8QBi0HnVF9o_P3PsCeH6XAwyQQh-23gs1Xn2tue-tCs9lYe84JXCSRJy1FQAnw';
 
 // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
 // like '/berry-material-react/react/default'
